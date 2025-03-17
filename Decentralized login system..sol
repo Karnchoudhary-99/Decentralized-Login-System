d58240e30c8554d1343ddef24f6f7abcea8d9fd5
Karnchoudhary-99 <karnkarwasra0@gmail.com>
2025-03-17 15:48:47 +0530

    Update Decentralized login system..sol

--- Decentralized login system..sol
+++ Decentralized login system..sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract DecentralizedLoginSystem {
+
+    struct Project {
+        string title;
+        string description;
+    }
+
+    Project public project;
+
+    constructor(string memory _title, string memory _description) {
+        project = Project(_title, _description);
+    }
+
+    function getProjectDetails() public view returns (string memory, string memory) {
+        return (project.title, project.description);
+    }
+
+    function updateProjectDetails(string memory _title, string memory _description) public {
+        project.title = _title;
+        project.description = _description;
+    }
+}
 
